b6bd10d9b2d9660d7de09f2e9a5413a934f807bf
cobdev <cobdev3@proton.me>
2025-04-24 17:51:39 -0500

    i did a thing

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
 const express = require("express");
 const { createServer } = require("http");
 const { Server } = require("socket.io");
+const spawn = require("child_process").spawn;
+
+const child = spawn("python", ["-u", "-i", "-q"]);
+
+child.on("close", (code) => {
+    console.log(code);
+});
+
+child.stdout.on("data", handleThing);
+
+child.stderr.on("data", handleThing);
+
+function handleThing(data) {
+    const output = data.toString("utf8");
+    if (output == ">>> ") return;
+    console.log(output);
+}
 
 const app = express();
 const httpServer = createServer(app);
@@ -19,7 +36,13 @@ io.on("connection", (socket) => {
     console.log("socket connected");
 
     socket.on("run", (code) => {
-        console.log(code);
+        // todo: sanitize input
+        child.stdin.write(
+            code
+                .substring(1, code.length - 1)
+                .replaceAll('\\"', '"')
+                .replaceAll("\\n", ";") + "\n"
+        );
     });
 });
 
